7f441f6170a5ee10cc447f5540e9d6429f8c0a77
Neeraj Kumar <55330971+Ryuk-me@users.noreply.github.com>
2020-07-30 20:57:07 +0530

    Button

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,24 +1,43 @@
 package com.example.diceroller
 
 import android.graphics.Color
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val rollBtn: Button = findViewById(R.id.btnRoll)
         btnRoll.setOnClickListener {
-            val rollBtn: Button = findViewById(R.id.btnRoll)
-            btnRoll.text = "Touched"
-            btnRoll.setTextColor(Color.BLACK)
-            btnRoll.setBackgroundColor(Color.RED)
+            rollDice()
+//            btnRoll.text = "Touched"
+//            btnRoll.setTextColor(Color.BLACK)
+//            btnRoll.setBackgroundColor(Color.RED)
 
         }
+    }
+
+    private fun rollDice() {
+        val randomDice = Random().nextInt(6) + 1
+        val diceShow = when (randomDice) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.imageV)
+        diceImage.setImageResource(diceShow)
+
     }
 
 }

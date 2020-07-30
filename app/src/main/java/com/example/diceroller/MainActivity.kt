6b51a98b4fe5730d942c14da642d7c3152c8453f
Neeraj Kumar <55330971+Ryuk-me@users.noreply.github.com>
2020-07-30 20:37:42 +0530

    Button

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.diceroller
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnRoll.setOnClickListener {
+            val rollBtn: Button = findViewById(R.id.btnRoll)
+            btnRoll.text = "Touched"
+            btnRoll.setTextColor(Color.BLACK)
+            btnRoll.setBackgroundColor(Color.RED)
+
+        }
     }
 
 }
